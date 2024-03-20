fd77794109dac5dc05c8dc705329a73a5350ddd6
Diarmuid Sheeran <dssheeran@outlook.com>
2024-03-20 20:55:25 +0000

    Set up home page and test on deployment

--- base/views.py
+++ base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    template = 'base/index.html'
+
+    return render(request, template)
--- games/settings.py
+++ games/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['retro-gaming-site-5328a324207f.herokuapp.com']
+ALLOWED_HOSTS = ['retro-gaming-site-5328a324207f.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
