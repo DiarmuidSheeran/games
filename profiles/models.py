24a2f571603d9f89a7ecb6f613ade12bd84edf57
Diarmuid Sheeran <dssheeran@outlook.com>
2024-03-25 13:10:27 +0000

    Add reciever to profile model to create user profile on user creation

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +9,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
