ebb37ac64b695acd5bd07698899d3648697a042d
Diarmuid Sheeran <dssheeran@outlook.com>
2024-03-26 21:15:03 +0000

    Edit index page to show games in development, creatre form and edit bio page to allow users to edit their bios

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
+    path('edit-bio/', views.edit_bio, name='edit_bio'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required  
 
 @login_required
 def profile_view(request):
     profile = Profile.objects.get(user=request.user)
     return render(request, 'profiles/profile.html', {'profile': profile})
+
+@login_required
+def edit_bio(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/edit_bio.html', {'form': form})
