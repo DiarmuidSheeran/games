04c69f012b238d6462c760a2b7ef07f7ec9d21fc
Diarmuid Sheeran <dssheeran@outlook.com>
2024-04-06 10:58:21 +0100

    Create model for pacman game scroes to be recorded

---
+++ pacman/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import PacmanGameScore
+
+class GameScoreForm(forms.ModelForm):
+    class Meta:
+        model = PacmanGameScore
+        fields = ['user_name', 'score']
