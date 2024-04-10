a3fbad964100aa8d1407af3ef29d0f8ad667c1c2
Diarmuid Sheeran <dssheeran@outlook.com>
2024-04-10 09:49:08 +0100

    Begin Work on snake game, Create snake game board and add food and snake to board. Add movement for snake around the board

---
+++ static/js/snake/snake.js
@@ -0,0 +1,48 @@
+const grid = document.querySelector('.grid');
+const width = 15;
+let currentSnakeIndex = 112;
+let currentFoodIndex = 90;
+
+for (let i = 0; i < width * width; i++) {
+    const square = document.createElement('div');
+    grid.appendChild(square);
+}
+
+const squares = Array.from(document.querySelectorAll('.grid div'));
+
+squares[currentSnakeIndex].classList.add('snake');
+squares[currentFoodIndex].classList.add('food');
+
+function moveSnake(e) {
+    squares[currentSnakeIndex].classList.remove('snake');
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentSnakeIndex % width !== 0) currentSnakeIndex -= 1;
+            break;
+        case 'a':
+            if (currentSnakeIndex % width !== 0) currentSnakeIndex -= 1;
+            break;
+        case 'ArrowRight':
+            if (currentSnakeIndex % width < width - 1) currentSnakeIndex += 1;
+            break;
+        case 'd':
+            if (currentSnakeIndex % width < width - 1) currentSnakeIndex += 1;
+            break;
+        case 'ArrowUp':
+            if (currentSnakeIndex - width >= 0) currentSnakeIndex -= width;
+            break;
+        case 'w':
+            if (currentSnakeIndex - width >= 0) currentSnakeIndex -= width;
+            break;
+        case 'ArrowDown':
+            if (currentSnakeIndex + width < width * width) currentSnakeIndex += width;
+            break;
+        case 's':
+            if (currentSnakeIndex + width < width * width) currentSnakeIndex += width;
+            break;
+    }
+    squares[currentSnakeIndex].classList.add('snake');
+}
+document.addEventListener('keydown', moveSnake);
+
+console.log(squares)
