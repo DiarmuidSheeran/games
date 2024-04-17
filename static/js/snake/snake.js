bda6671f5651b5391750842399a0ed62226a1e84
Diarmuid Sheeran <dssheeran@outlook.com>
2024-04-17 13:12:16 +0100

    Complete draw snake function to add snake to the game board

--- static/js/snake/snake.js
+++ static/js/snake/snake.js
@@ -1,5 +1,7 @@
 let lastRenderTime = 0
 const SNAKE_SPEED = 2
+const snakeBody = [{x: 11, y: 11}]
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
@@ -8,6 +10,23 @@ function main(currentTime) {
 
     console.log('Render')
     lastRenderTime = currentTime
+
+    update()
+    draw()
 }
 
 window.requestAnimationFrame(main)
+
+function update(){
+    console.log('update snake')
+}
+
+function draw(){
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x
+        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
+}
