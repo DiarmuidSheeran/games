6c828ec3db02a3fd4d716439a2445ee3389839be
Diarmuid Sheeran <dssheeran@outlook.com>
2024-04-17 13:21:20 +0100

    Edit Update function to move snake body to the left of game board

--- static/js/snake/snake.js
+++ static/js/snake/snake.js
@@ -1,6 +1,8 @@
 let lastRenderTime = 0
 const SNAKE_SPEED = 2
-const snakeBody = [{x: 11, y: 11}]
+const snakeBody = [
+    {x: 11, y: 11}
+]
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
@@ -18,14 +20,20 @@ function main(currentTime) {
 window.requestAnimationFrame(main)
 
 function update(){
-    console.log('update snake')
+    for (let i = snakeBody.length -2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i]}
+    }
+
+    snakeBody[0].x += 1
+    snakeBody[0].y += 0
 }
 
 function draw(){
+    gameBoard.innerHTML = ""
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div')
-        snakeElement.style.gridRowStart = segment.x
-        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.style.gridColumnStart = segment.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
     })
